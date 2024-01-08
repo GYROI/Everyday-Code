ac586575e81b3e2d6d3a3568e96f6568e1bb9659
GYROI <118619281+GYROI@users.noreply.github.com>
2024-01-08 17:00:18 +0700

    Sky+Box

---
+++ SKYBOX.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#define SCREENW 800
+#define SCREENH 600
+#undef main
+using namespace std;
+
+class RECT {
+public:
+    int Posx, Posy, w, h;
+    RECT(int x, int y, int width, int height) : Posx(x), Posy(y), w(width), h(height) {}
+};
+
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+
+// Function to load a texture from file
+SDL_Texture* loadTexture(const std::string& filePath) {
+    SDL_Texture* texture = nullptr;
+    SDL_Surface* surface = SDL_LoadBMP(filePath.c_str());
+    if (surface == nullptr) {
+        cout << "Failed to load image: " << SDL_GetError() << endl;
+    } else {
+        texture = SDL_CreateTextureFromSurface(renderer, surface);
+        if (texture == nullptr) {
+            cout << "Failed to create texture: " << SDL_GetError() << endl;
+        }
+        SDL_FreeSurface(surface);
+    }
+    return texture;
+}
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+        return -1;
+    }
+
+    window = SDL_CreateWindow("SDL Texture Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        SCREENW, SCREENH, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+        return -1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
+        return -1;
+    }
+
+    // Load a texture
+    SDL_Texture* texture = loadTexture("D:/sky-mvehfqz6w2ges2dj.bmp");
+    if (texture == nullptr) {
+        cout << "Failed to load texture!" << endl;
+        return -1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    int width = 100;
+    int height = 100;
+    int recX = (SCREENW - width)/2;
+    int recY = (SCREENH - height)/2;
+    RECT meRect(recX, recY, width, height);
+
+    while (!quit) {
+         while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT || e.key.keysym.sym == SDLK_ESCAPE) {
+                quit = true;
+            } else if(e.type == SDL_KEYDOWN){
+                if(e.key.keysym.sym == SDLK_w){
+                    width += 10;
+                } else if(e.key.keysym.sym == SDLK_h){
+                    height += 10;
+                } else if(e.key.keysym.sym == SDLK_a){
+                    width -= 10;
+                } else if(e.key.keysym.sym == SDLK_z){
+                    height-=10;
+                }
+            }
+        }
+            meRect.w = width;
+            meRect.h = height;
+        // Clear the renderer
+        SDL_RenderClear(renderer);
+
+        // Render the texture to the screen
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+        SDL_Rect rect = { meRect.Posx, meRect.Posy, meRect.w, meRect.h };
+        SDL_RenderFillRect(renderer, &rect);
+        // Update the screen
+        SDL_RenderPresent(renderer);
+    }
+
+    // Cleanup
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
