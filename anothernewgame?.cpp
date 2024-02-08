fca4b70b7d3214c639d58d28364560439951e2bb
GYROI <118619281+GYROI@users.noreply.github.com>
2024-02-08 13:29:52 +0700

    It is done

--- anothernewgame?.cpp
+++ anothernewgame?.cpp
@@ -11,6 +11,9 @@ using namespace std;
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 
+SDL_Rect rectdot = {0, 0, 50, 50};
+SDL_Rect wallrect = {650, 100, 200, 500};
+
 SDL_Texture* loadTexture(const std::string& filePath, SDL_Renderer* renderer) {
     SDL_Texture* texture = nullptr;
     SDL_Surface* surface = SDL_LoadBMP(filePath.c_str());
@@ -54,64 +57,71 @@ void quitSDL() {
     SDL_Quit();
 }
 
-SDL_Rect rectdot = {0, 0, 50, 50};
-SDL_Rect wallrect = {650, 100, 200, 500};
+void capFrameRate(Uint32 startTicks) {
+    const int SCREEN_FPS = 60;
+    const int SCREEN_TICKS_PER_FRAME = 1000 / SCREEN_FPS;
 
-uint32_t movement = 5;
+    Uint32 currentTicks = SDL_GetTicks();
+    Uint32 frameTicks = currentTicks - startTicks;
 
-bool checkWindowCollision(const SDL_Rect& rect, int windowWidth, int windowHeight) {
-    return (
-        rect.x >= 0 &&
-        rect.y >= 0 &&
-        rect.x + rect.w <= windowWidth &&
-        rect.y + rect.h <= windowHeight
-    );
+    if (frameTicks < SCREEN_TICKS_PER_FRAME) {
+        SDL_Delay(SCREEN_TICKS_PER_FRAME - frameTicks);
+    }
 }
+
+void loadNextWindow(SDL_Renderer* renderer, SDL_Texture*& oldBg, SDL_Texture* newBG, SDL_Texture*& dot, SDL_Texture*& wall, SDL_Rect dotrect) {
+    // Load new textures
+    SDL_Texture* newWall = loadTexture("C://Users//User//Desktop//SDL//wudooooo//gametext//wall.bmp", renderer);
+
+    // Render new scene
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, newBG, nullptr, nullptr);
+    SDL_RenderCopy(renderer, dot, nullptr, &dotrect);
+    SDL_RenderCopy(renderer, newWall, nullptr, &wallrect);
+    SDL_RenderPresent(renderer);
+
+    // Destroy old wall texture
+    SDL_DestroyTexture(wall);
+
+    // Update textures
+    oldBg = newBG;
+    wall = newWall;
+}
+
+uint32_t movement = 10;
 
 int main() {
     initSDL();
 
     SDL_Event e;
     bool isRunning = true;
-    SDL_Texture* bg = loadTexture("D://background.bmp", renderer);
-    SDL_Texture* dot = loadTexture("D://dot.bmp", renderer);
-    SDL_Texture* wall = loadTexture("D://wall.bmp", renderer);
-    if (bg == nullptr || dot == nullptr || wall == nullptr) {
+    SDL_Texture* bg = loadTexture("C://Users//User//Desktop//SDL//wudooooo//gametext//background.bmp", renderer);
+    SDL_Texture* dot = loadTexture("C://Users//User//Desktop//SDL//wudooooo//gametext//dot.bmp", renderer);
+    SDL_Texture* wall = loadTexture("C://Users//User//Desktop//SDL//wudooooo//gametext//wall.bmp", renderer);
+    SDL_Texture* newBg = loadTexture("C://Users//User//Desktop//SDL//wudooooo//gametext//sky.bmp", renderer);
+    if (bg == nullptr || dot == nullptr || wall == nullptr || newBg == nullptr) {
         cout << "IMAGE CAN'T BE LOADED" << SDL_GetError() << endl;
         isRunning = false;
     }
 
     while (isRunning) {
+        Uint32 startTicks = SDL_GetTicks();
+
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
                 isRunning = false;
             } else if (e.type == SDL_KEYDOWN) {
-               switch (e.key.keysym.sym) {
-                case SDLK_ESCAPE:
+                if(e.key.keysym.sym == SDLK_ESCAPE){
                     isRunning = false;
-                    break;
-                case SDLK_a:
-                    if (checkWindowCollision(rectdot, SCREEN_WIDTH, SCREEN_HEIGHT)) {
-                        rectdot.x -= movement;
-                    }
-                    break; // Add break statement here
-                case SDLK_d:
-                    if (checkWindowCollision(rectdot, SCREEN_WIDTH, SCREEN_HEIGHT)) {
-                        rectdot.x += movement;
-                    }
-                    break; // Add break statement here
-                case SDLK_w:
-                    if (checkWindowCollision(rectdot, SCREEN_WIDTH, SCREEN_HEIGHT)) {
-                        rectdot.y -= movement;
-                    }
-                    break; // Add break statement here
-                case SDLK_s:
-                    if (checkWindowCollision(rectdot, SCREEN_WIDTH, SCREEN_HEIGHT)) {
-                        rectdot.y += movement;
-                    }
-                    break; // Add break statement here
+                } else if(e.key.keysym.sym == SDLK_a){
+                    rectdot.x -= movement;
+                } else if(e.key.keysym.sym == SDLK_d){
+                    rectdot.x += movement;
+                } else if(e.key.keysym.sym == SDLK_w){
+                    rectdot.y -= movement;
+                } else if(e.key.keysym.sym == SDLK_s){
+                    rectdot.y += movement;
                 }
-
             }
         }
 
@@ -119,9 +129,24 @@ int main() {
         SDL_RenderCopy(renderer, bg, nullptr, nullptr);
         SDL_RenderCopy(renderer, dot, nullptr, &rectdot);
         SDL_RenderCopy(renderer, wall, nullptr, &wallrect);
+
+        if (rectdot.x < wallrect.x + wallrect.w && rectdot.x + rectdot.w > wallrect.x && rectdot.y < wallrect.y + wallrect.h &&
+            rectdot.y + rectdot.h > wallrect.y)
+        {
+            loadNextWindow(renderer, bg, newBg, dot, wall, rectdot);
+            SDL_RenderPresent(renderer);
+            SDL_Delay(100);
+        }
+
         SDL_RenderPresent(renderer);
+
+        capFrameRate(startTicks);
     }
 
+    SDL_DestroyTexture(bg);
+    SDL_DestroyTexture(dot);
+    SDL_DestroyTexture(wall);
+    SDL_DestroyTexture(newBg);
     quitSDL();
     return 0;
 }
