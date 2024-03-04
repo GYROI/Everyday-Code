44d714b8542c9f85093b2eb6345cb78e24a76563
GYROI <118619281+GYROI@users.noreply.github.com>
2024-03-04 18:27:57 +0700

    Update newg.cpp

--- newg.cpp
+++ newg.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #define SCREENW 800
 #define SCREENH 600
+const int JUMPH = 100;
+const int Gravity = 5;
 using namespace std;
 #undef main
 
@@ -10,7 +12,7 @@ void Game::initSDL(SDL_Window*& window, SDL_Renderer*& renderer) {
         cout << "SDL CANT BE INITIATED" << SDL_GetError() << endl;
     }
 
-    window = SDL_CreateWindow("Game using classes", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREENW, SCREENH, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Just Mario", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREENW, SCREENH, SDL_WINDOW_SHOWN);
     if(window == nullptr){
         cout << "Window cant be made" << SDL_GetError() << endl;
     }
@@ -51,6 +53,17 @@ void Game::returnMario(SDL_Rect& marioRect){
         marioRect.x = 0 - marioRect.w;
     }
 }
+
+bool Game::checkCol(SDL_Rect& P, SDL_Rect& B) {
+    // Check for horizontal collision
+    bool horizontalCollision = (P.x + P.w) <= B.x || P.x >= (B.x + B.w);
+
+    // Check for vertical collision
+    bool verticalCollision = (P.y + P.h) <= B.y || P.y >= (B.y + B.h);
+
+    // Return true if there is no collision (both horizontal and vertical)
+    return !(horizontalCollision || verticalCollision);
+}
 void Game::close(SDL_Window*& window, SDL_Renderer*& renderer) {
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
@@ -60,9 +73,10 @@ void Game::close(SDL_Window*& window, SDL_Renderer*& renderer) {
 int main(){
     SDL_Window* window;
     SDL_Renderer* renderer;
-    SDL_Rect marioSize = {0, 473, 74, 53};
+    SDL_Rect marioSize = {0, 473, 51, 53};
     SDL_Rect landS = {0, 526, 812, 90};
     SDL_Color windowCl = {255,255,255};
+    SDL_Rect blockS = {374, 457, 52, 69};
 
     Game meGame;
 
@@ -70,9 +84,12 @@ int main(){
 
     SDL_Event e;
     bool isRunning = true;
+    bool isJumping = false;
+    int jumpH = 0;
 
     SDL_Texture* texture = meGame.loadTexture(renderer, "C:/Users/User/Desktop/SDL/mario/textures/t1.bmp");
     SDL_Texture* land = meGame.loadTexture(renderer, "C:/Users/User/Desktop/SDL/mario/textures/land.bmp");
+    SDL_Texture* block = meGame.loadTexture(renderer, "C:/Users/User/Desktop/SDL/mario/textures/block.bmp");
 
     while(isRunning){
         Uint32 starticks = SDL_GetTicks();
@@ -82,15 +99,31 @@ int main(){
             } else if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE){
                 isRunning = false;
             } else if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RIGHT){
-                marioSize.x += 10;
-                meGame.returnMario(marioSize);
+                if(!meGame.checkCol(marioSize, blockS)){
+                     marioSize.x += 10;
+                     meGame.returnMario(marioSize);
+                }
+            } else if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE && !isJumping){
+                isJumping = true;
+                jumpH = -40;
             }
-
         }
+
+        if (isJumping && !meGame.checkCol(marioSize, blockS) ) {
+            marioSize.y += jumpH;
+            marioSize.x += 10;
+            jumpH += Gravity;
+            if (marioSize.y >= 473) {
+                marioSize.y = 473;
+                isJumping = false; // Reset jump state when on the ground
+        }
+    }
+
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, texture, NULL, &marioSize);
         SDL_SetRenderDrawColor(renderer, windowCl.r, windowCl.g, windowCl.b, 255);
         SDL_RenderCopy(renderer, land, NULL, &landS);
+        SDL_RenderCopy(renderer, block, NULL, &blockS);
         SDL_RenderPresent(renderer);
 
         meGame.capFrame(starticks);
